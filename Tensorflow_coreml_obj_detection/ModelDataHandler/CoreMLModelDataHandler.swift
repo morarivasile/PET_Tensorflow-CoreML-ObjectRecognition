c16fcdbe9b8be789d3b145ae02dd8119b4b9a896
Vasile Morari <>
2020-02-20 13:36:55 +0200

    FIxed colors for classes.

--- Tensorflow_coreml_obj_detection/ModelDataHandler/CoreMLModelDataHandler.swift
+++ Tensorflow_coreml_obj_detection/ModelDataHandler/CoreMLModelDataHandler.swift
@@ -11,30 +11,17 @@ import Vision
 
 class CoreMLModelDataHandler: NSObject, ModelDataHandler {
     
+    private let mlModel: MLModel
     private let vnCoreMLModel: VNCoreMLModel
     
-    private let colorStrideValue = 10
-    
     private let drawRectSize: CGSize
-    
-    private let colors = [
-        UIColor.red,
-        UIColor(displayP3Red: 90.0/255.0, green: 200.0/255.0, blue: 250.0/255.0, alpha: 1.0),
-        UIColor.green,
-        UIColor.orange,
-        UIColor.blue,
-        UIColor.purple,
-        UIColor.magenta,
-        UIColor.yellow,
-        UIColor.cyan,
-        UIColor.brown
-    ]
     
     init?(mlModel: MLModel, drawRectSize: CGSize) {
         guard let vnCoreMLModel = try? VNCoreMLModel(for: mlModel) else {
             return nil
         }
         
+        self.mlModel = mlModel
         self.vnCoreMLModel = vnCoreMLModel
         self.drawRectSize = drawRectSize
     }
@@ -67,52 +54,47 @@ class CoreMLModelDataHandler: NSObject, ModelDataHandler {
             
             // Select only the label with the highest confidence.
             let topLabelObservation = objectObservation.labels[0]
-            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(width), Int(height))
-            let color = colorForClass(withIndex: index + 1)
+            let color = colorForClass(withLabel: topLabelObservation.identifier)
             
-            var newRect = objectObservation.boundingBox.applying(CGAffineTransform(scaleX: CGFloat(width), y: CGFloat(height)))
+            var objectBounds = objectObservation.boundingBox.applying(CGAffineTransform(scaleX: CGFloat(width), y: CGFloat(height)))
             
-            newRect.origin.y = CGFloat(height) - newRect.origin.y - newRect.height
+            objectBounds.origin.y = CGFloat(height) - objectBounds.origin.y - objectBounds.height
             
-            resultArray.append(Inference(confidence: topLabelObservation.confidence, className: topLabelObservation.identifier, rect: newRect, displayColor: color))
+            resultArray.append(Inference(confidence: topLabelObservation.confidence, className: topLabelObservation.identifier, rect: objectBounds, displayColor: color))
         }
         
         return Result(inferences: resultArray)
     }
     
-    /// This assigns color for a particular class.
-    private func colorForClass(withIndex index: Int) -> UIColor {
-        // We have a set of colors and the depending upon a stride, it assigns variations to of the base
-        // colors to each object based on its index.
-        let baseColor = colors[index % colors.count]
+    public func exifOrientationFromDeviceOrientation() -> CGImagePropertyOrientation {
+        let curDeviceOrientation = UIDevice.current.orientation
+        let exifOrientation: CGImagePropertyOrientation
         
-        var colorToAssign = baseColor
-        
-        let percentage = CGFloat((colorStrideValue / 2 - index / colors.count) * colorStrideValue)
-        
-        if let modifiedColor = baseColor.getModified(byPercentage: percentage) {
-            colorToAssign = modifiedColor
+        switch curDeviceOrientation {
+        case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
+            exifOrientation = .left
+        case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
+            exifOrientation = .upMirrored
+        case UIDeviceOrientation.landscapeRight:      // Device oriented horizontally, home button on the left
+            exifOrientation = .down
+        case UIDeviceOrientation.portrait:            // Device oriented vertically, home button on the bottom
+            exifOrientation = .up
+        default:
+            exifOrientation = .up
         }
-        
-        return colorToAssign
+        return exifOrientation
     }
     
-    public func exifOrientationFromDeviceOrientation() -> CGImagePropertyOrientation {
-           let curDeviceOrientation = UIDevice.current.orientation
-           let exifOrientation: CGImagePropertyOrientation
-           
-           switch curDeviceOrientation {
-           case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
-               exifOrientation = .left
-           case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
-               exifOrientation = .upMirrored
-           case UIDeviceOrientation.landscapeRight:      // Device oriented horizontally, home button on the left
-               exifOrientation = .down
-           case UIDeviceOrientation.portrait:            // Device oriented vertically, home button on the bottom
-               exifOrientation = .up
-           default:
-               exifOrientation = .up
-           }
-           return exifOrientation
-       }
+    private func getLabelsList() -> [String] {
+        guard let userDefined = mlModel.modelDescription.metadata[MLModelMetadataKey.creatorDefinedKey] as? [String : String] else {
+            print("Failed to retrieve mlModel creatorDefinedKey.")
+            return []
+        }
+        return userDefined["classes"]?.components(separatedBy: ",") ?? []
+    }
+    
+    private func colorForClass(withLabel label: String) -> UIColor {
+        let classIndex = mlModel.classes.firstIndex(of: label) ?? 1
+        return UIColor.colorForClass(withIndex: classIndex)
+    }
 }
